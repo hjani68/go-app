29ba9583e7fbb5c63bc833d748d06e0de4d5f20b
hjani68 <hardik.developer@gmail.com>
2021-06-05 13:18:13 +1000

    deal - return multiple values

--- Cards/deck.go
+++ Cards/deck.go
@@ -9,7 +9,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clunbs"} // slice of strings
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"} // slice of strings
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
@@ -31,4 +31,13 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+//deal function
+// type deck d
+// handSize : here we use 3 so used int type
+//returning multiple parameters and both type deck
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:] // return two slices (one from start to handsize(nothig before) and second UptohandSize(nothing after))
+
 }
--- Cards/main.go
+++ Cards/main.go
@@ -3,5 +3,9 @@ package main
 func main() {
 
 	cards := newDeck()
-	cards.print()
+
+	hand, remainingCards := deal(cards, 5) // two return values, first assigned to hand variable and second assigned to remaingCards variable
+	hand.print()
+	remainingCards.print()
+
 }
